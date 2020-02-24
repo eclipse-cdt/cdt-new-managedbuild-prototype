7f47f16dfa868c9f545e03fe354781e1a0a0c1ac
William Riley <william.riley@renesas.com>
2020-02-24 19:17:05 +0000

    Add framework plugin & empty Ninja pluginAdd placeholder plugins for framework and Ninja integration

---
+++ bundles/org.eclipse.cdt.build.managed.ninja/src/org/eclipse/cdt/build/managed/ninja/NinjaBuildEngine.java
@@ -0,0 +1,11 @@
+package org.eclipse.cdt.build.managed.ninja;
+
+import org.eclipse.cdt.build.managed.framework.buildengine.IBuildEngine;
+
+public class NinjaBuildEngine implements IBuildEngine {
+
+	public NinjaBuildEngine() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
